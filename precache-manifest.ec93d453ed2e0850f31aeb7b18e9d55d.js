0f095abcfe762b829dbca9a45f6d6339a405f3e7
brakahaugen <espenbragerhaug@gmail.com>
2021-06-30 00:38:17 +0200

    Updates

---
+++ precache-manifest.ec93d453ed2e0850f31aeb7b18e9d55d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85052c94e3cc3d57a51616906d96ca0c",
+    "url": "/stange_map/index.html"
+  },
+  {
+    "revision": "c3cac477bfc24905b946",
+    "url": "/stange_map/static/css/2.fbf1415f.chunk.css"
+  },
+  {
+    "revision": "a2dcd2437ca284da3f93",
+    "url": "/stange_map/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3cac477bfc24905b946",
+    "url": "/stange_map/static/js/2.23781885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stange_map/static/js/2.23781885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2dcd2437ca284da3f93",
+    "url": "/stange_map/static/js/main.f12026c7.chunk.js"
+  },
+  {
+    "revision": "991f5cdf8921c0e72d4c",
+    "url": "/stange_map/static/js/runtime-main.7d5bb015.js"
+  },
+  {
+    "revision": "52272777e017076f4e39caca6561fd4b",
+    "url": "/stange_map/static/media/norkart.52272777.jpg"
+  }
+]);
